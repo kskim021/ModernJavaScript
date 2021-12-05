eabbbdd1bb0860afb50c997add84fcd8388191b3
kskim021 <kskim@sinsabridge.com>
2021-12-05 22:11:53 +0900

    3차 모던자바스크립트 정리

--- javascript/myapp/index.js
+++ javascript/myapp/index.js
@@ -1,6 +1,26 @@
 const arr = [1,2,3];
 
-arr.forEach(console.log)
+// arr.forEach(console.log)
 
 //alert("a")  //브라우저서만 동작하는 클라이언트 사이트 Web API
 
+// console.log(score);
+// score = 80;
+// var score;
+// console.log(score);
+
+
+// var score = 80;  // 변수 선언
+// score = 90; //  재할당
+
+
+// const  // 재할당 불가
+
+let t = 80;
+t = 90;
+console.log(t);
+
+
+const name = '관석';
+console.log(`my name is ${name}`);
+console.log('my name is ' + name);
